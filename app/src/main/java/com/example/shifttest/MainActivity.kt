116e7f8fad3730fcbd80c4647e10389bc581c9c8
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-03-06 20:27:15 +0700

    установил listener

--- app/src/main/java/com/example/shifttest/MainActivity.kt
+++ app/src/main/java/com/example/shifttest/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.shifttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.shifttest.data.BinRepository
+import com.example.shifttest.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private val repository = BinRepository()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBtnListener()
     }
+
+    private fun setBtnListener(){
+        binding.run{
+            searchBtn.setOnClickListener {
+                val binNum = editBin.text.toString().toInt()
+                lifecycleScope.launch {
+                    val binInfo = repository.getByNum(binNum)
+                    textView2.text = binInfo.scheme
+                }
+            }
+        }
+    }
+
+
 }
